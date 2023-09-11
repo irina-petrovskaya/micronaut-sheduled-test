a8a1707700d2e48a1b29f3d41c29832502bf564e
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2023-09-11 16:52:14 +0200

    update micronaut version

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -21,6 +21,7 @@ public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
+
     @EventListener
     @Async
     public void onStartup(ServerStartupEvent event) {
